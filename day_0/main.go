0d076a6ac40ac613304bc960ae0a72326d675a24
Bubunyo Nyavor <kidd.bubu@gmail.com>
2024-12-03 08:50:24 +0100

    day3

--- day_0/main.go
+++ day_0/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"bubunyo/aoc-25/common"
 )
+
+const prefix = "day_3"
 
 func main() {
 	prefix := "Day 1:"
@@ -15,9 +20,14 @@ func main() {
 }
 
 func run_(fp string) any {
-	for str := range common.IterateFileContent("day_0/" + fp) {
-		_ = str
-	}
 	acc := 0
+	for str := range common.IterateFileContent(prefix + "/" + fp) {
+		s := strings.Split(str, " ")
+		i, err := strconv.Atoi(s[0])
+		if err != nil {
+			log.Fatalln("li", err)
+		}
+		_ = i
+	}
 	return acc
 }
--- day_2/main.go
+++ day_2/main.go
@@ -2,22 +2,194 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"bubunyo/aoc-25/common"
 )
 
+const prefix = "day_2"
+
 func main() {
-	prefix := "Day 1:"
-	fmt.Println(prefix, "Demo 1-", run_("input_demo"))
-	fmt.Println(prefix, "Run 1 ->", run_("input"))
-	fmt.Println(prefix, "Demo 2 ->", run_("input_demo"))
-	fmt.Println(prefix, "Run 2 ->", run_("input"))
+	// fmt.Println(prefix, "Demo 1::", run1("input_demo"))
+	// fmt.Println(prefix, "Run 1 ->", run1("input"))
+	fmt.Println(prefix, "Demo 2 ->", run2("input_demo"))
+	fmt.Println(prefix, "Run 2 ->", run2("input")) // 663 -> high, 650 - low, 658 -- answer
 }
 
-func run_(fp string) any {
-	for str := range common.IterateFileContent("day_0/" + fp) {
-		_ = str
-	}
+func run2(fp string) any {
 	acc := 0
+	for str := range common.IterateFileContent(prefix + "/" + fp) {
+		_ = str
+		report := strings.Split(str, " ")
+
+		prev, err := strconv.Atoi(report[0])
+		if err != nil {
+			log.Fatalln("strconv:", err)
+		}
+
+		level := 0 // 0 for unset,  1 for increasing, -1 for decreasing
+
+		safe := true
+		dampCount := 0
+
+		for i := 1; i < len(report); i++ {
+			curr, err := strconv.Atoi(report[i])
+
+			if err != nil {
+				log.Fatalln("strconv:", err)
+			}
+
+			if curr == prev {
+				dampCount++
+				if dampCount > 1 {
+					safe = false
+					break
+				}
+				continue
+			}
+
+			if level == 0 {
+				if curr > prev {
+					level = 1 // increasing
+				} else {
+					level = -1 // decreasing
+				}
+			}
+
+			var amt int
+			if level > 0 {
+				amt = curr - prev
+			} else {
+				amt = prev - curr
+			}
+
+			if amt <= 0 || amt > 3 {
+				dampCount++
+				if dampCount > 1 {
+					safe = false
+					break
+				}
+				level = 0
+				continue
+			}
+			prev = curr
+		}
+		if !safe {
+			prev, err := strconv.Atoi(report[len(report)-1])
+			if err != nil {
+				log.Fatalln("strconv:", err)
+			}
+
+			level = 0 // 0 for unset,  1 for increasing, -1 for decreasing
+
+			safe = true
+			dampCount = 0
+
+			for i := len(report) - 2; i >= 0; i-- {
+				curr, err := strconv.Atoi(report[i])
+
+				if err != nil {
+					log.Fatalln("strconv:", err)
+				}
+
+				if curr == prev {
+					dampCount++
+					if dampCount > 1 {
+						safe = false
+						break
+					}
+					continue
+				}
+
+				if level == 0 {
+					if curr > prev {
+						level = 1 // increasing
+					} else {
+						level = -1 // decreasing
+					}
+				}
+
+				var amt int
+				if level > 0 {
+					amt = curr - prev
+				} else {
+					amt = prev - curr
+				}
+
+				if amt <= 0 || amt > 3 {
+					dampCount++
+					if dampCount > 1 {
+						safe = false
+						break
+					}
+					level = 0
+					continue
+				}
+				prev = curr
+			}
+		}
+
+		if safe {
+			acc++
+		}
+	}
+	return acc
+}
+
+func run1(fp string) any {
+	acc := 0
+	for str := range common.IterateFileContent(prefix + "/" + fp) {
+		_ = str
+		report := strings.Split(str, " ")
+
+		prev, err := strconv.Atoi(report[0])
+		if err != nil {
+			log.Fatalln("strconv:", err)
+		}
+
+		level := 0 // 0 for unset,  1 for increasing, -1 for decreasing
+
+		safe := true
+
+		for i := 1; i < len(report); i++ {
+			curr, err := strconv.Atoi(report[i])
+
+			if err != nil {
+				log.Fatalln("strconv:", err)
+			}
+
+			if curr == prev {
+				safe = false
+				break
+			}
+
+			// set level only on first iteration
+			if i == 1 {
+				if curr > prev {
+					level = 1 // increasing
+				} else {
+					level = -1 // decreasing
+				}
+			}
+
+			var amt int
+			if level > 0 {
+				amt = curr - prev
+			} else {
+				amt = prev - curr
+			}
+
+			if amt <= 0 || amt > 3 {
+				safe = false
+				break
+			}
+			prev = curr
+		}
+		if safe {
+			acc++
+		}
+	}
 	return acc
 }
